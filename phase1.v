c8808c7e94bcbc75fdbf5b4871e5fb142cf23b58
galgx15 <galgx15@gmail.com>
2019-04-10 18:53:14 +0300

    take the 8 most significate bit of the inner product

--- phase1.v
+++ phase1.v
@@ -24,15 +24,14 @@ module phase1(x,teta,h);
     input [63:0] x;
     input [63:0] teta;
     output [7:0] h;
-    `include "dot_product.v";
+   // `include "dot_product.v";
+    
     reg [18:0] inner_product;
-    wire [9:0] z;
-    dot_product(inp1(x), inp2(teta),outp(inner_product));
-    genvar i;
-    generate
-         for (i = 0; i < 10; i = i + 1) begin
-           assign z[i] = inner_product[i*2];  
-        end
-    endgenerate
+    wire [7:0] z;
+    dot_product idot_product ( .inp1(x), .inp2(teta), .outp(inner_product));
+
+    assign z = inner_product[63:(63-8)];  
+    sigmoid_LUT isigmoid_LUT ( .z(z), .h(h));
+   
     
 endmodule
